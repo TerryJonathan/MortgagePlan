21ef3b920771d9ae65913590d81896eca5f6b6bb
TerryJonathan <jonathan.m.terry@hotmail.com>
2022-12-25 20:02:21 +0200

    Initial commit. Created spring project in intelliJ with dependencies.

---
+++ src/main/java/ax/codetest/mortgageplanweb/MortgagePlanWebApplication.java
@@ -0,0 +1,13 @@
+package ax.codetest.mortgageplanweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgagePlanWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MortgagePlanWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ax/codetest/mortgageplanweb/MortgagePlanWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ax.codetest.mortgageplanweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgagePlanWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
