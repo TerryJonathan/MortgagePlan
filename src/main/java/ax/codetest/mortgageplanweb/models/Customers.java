2eec41ff2c2b9dae34c7dafe60fd3a6bbc988fea
TerryJonathan <jonathan.m.terry@hotmail.com>
2022-12-26 18:46:05 +0200

    added calculateMonthlyPayment method. When a new customer is added/ created calculation is performed

--- src/main/java/ax/codetest/mortgageplanweb/models/Customers.java
+++ src/main/java/ax/codetest/mortgageplanweb/models/Customers.java
@@ -36,8 +36,31 @@ public class Customers {
         this.loan= loan;
         this.interestRate= interestRate;
         this.numberOfYears= numberOfYears;
-        // paymentCalculator
+        this.monthlyPayment= calculateMonthlyPayment(loan, interestRate, numberOfYears);
     }
+    private double pow(double base, int power){
+        double result=1;
+        for (int i = power; i !=0 ; i--) {
+            result= result * base;
+        }
+        return result;
+    }
+    private double calculateMonthlyPayment(double loan, double interestRate, int numberOfYears) {
+        // p = Number of payments   |   b = Interest on a monthly basis
+        // U = Total loan           |   E = Fixed monthly payment
+        // x = (1 + b)^p
+
+        // p = Number of payments
+        int numberOfPayments= numberOfYears*12;
+        // b = Interest on a monthly basis
+        double monthlyInterest=(interestRate/100)/12;
+        //Original formula:  E = U[b(1 + b)^p]/[(1 + b)^p - 1]
+        double x= pow(monthlyInterest+1,numberOfPayments); // (1+b)^p == x
+
+        //Simplified formula:  E = U[bx]/[x - 1]
+        return loan* (monthlyInterest*x) / (x-1);
+    }
+
 
     @Override
     public String toString(){
