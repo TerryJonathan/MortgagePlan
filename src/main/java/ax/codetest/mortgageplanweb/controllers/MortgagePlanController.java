fe6b05e5bc2d58fc128c926764770d960bc47ed2
TerryJonathan <jonathan.m.terry@hotmail.com>
2022-12-26 20:04:13 +0200

    Added table to index and fetched customer content from h2 database

--- src/main/java/ax/codetest/mortgageplanweb/controllers/MortgagePlanController.java
+++ src/main/java/ax/codetest/mortgageplanweb/controllers/MortgagePlanController.java
@@ -1,7 +1,9 @@
 package ax.codetest.mortgageplanweb.controllers;
 
+import ax.codetest.mortgageplanweb.repositories.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,11 +13,17 @@ public class MortgagePlanController {
     // Logger
     private final Logger logger = LoggerFactory.getLogger(MortgagePlanController.class);
     // Mapping to index in templates on GET request
+    @Autowired
+    private CustomerRepository customerRepository;
     @GetMapping("/")
     public ModelAndView index(){
         logger.debug("GET index");
 
         ModelAndView modelAndView= new ModelAndView("index");
+        // Adding customers to modelAndView
+        // pulling from database and creating a "list object" with the key name of customers
+        // this makes it possible to in View template to hav access to this list.
+        modelAndView.addObject("customerEntries",customerRepository.findAll());
         return modelAndView;
     }
 
